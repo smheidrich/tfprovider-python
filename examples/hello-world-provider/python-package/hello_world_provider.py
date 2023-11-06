01df5023fc7d1a0dfbeccbc3253796b31d37db9a
Shahriar Heidrich <smheidrich@weltenfunktion.de>
2023-11-06 01:09:18 +0100

    Make provider servicer impl part of example
    Might come back into main project to provide a more high-level
    abstraction, but for experimenting it makes more sense to get started
    with examples that cover the relevant features.

--- examples/hello-world-provider/python-package/hello_world_provider.py
+++ examples/hello-world-provider/python-package/hello_world_provider.py
@@ -1,9 +1,39 @@
 from tfprovider.rpc_plugin import RPCPluginServer
+from tfprovider.tfplugin64_pb2 import (
+    GetMetadata,
+    GetProviderSchema,
+    Schema,
+    ServerCapabilities,
+    StringKind,
+)
+from tfprovider.tfplugin64_pb2_grpc import ProviderServicer
+
+
+class ProviderServicer(ProviderServicer):
+    def GetMetadata(self, request, context):
+        return GetMetadata.Response(
+            server_capabilities=ServerCapabilities(
+                plan_destroy=False, get_provider_schema_optional=False
+            ),
+        )
+
+    def GetProviderSchema(self, request, context):
+        return GetProviderSchema.Response(
+            provider=Schema(
+                version=1,
+                block=Schema.Block(
+                    version=1,
+                    description="Hello World provider",
+                    description_kind=StringKind.PLAIN,
+                ),
+            ),
+        )
 
 
 def main():
-    s = RPCPluginServer()
+    s = RPCPluginServer(ProviderServicer())
     s.run()
+
 
 if __name__ == "__main__":
     main()
--- tfprovider/rpc_plugin.py
+++ tfprovider/rpc_plugin.py
@@ -16,22 +16,23 @@ from cryptography.x509.oid import NameOID
 from . import grpc_controller_pb2_grpc, tfplugin64_pb2_grpc
 from .controller_servicer import ControllerServicer
 from .health_servicer import _configure_health_server
-from .provider_servicer import ProviderServicer
 
 
 class RPCPluginServer:
-    def __init__(self):
+    def __init__(
+        self,
+        provider_servicer: tfplugin64_pb2_grpc.ProviderServicer,
+        port: str = "1234",
+    ):
+        self.port = port
         self.cert, self.key = generate_server_cert()
-
-    def run(self):
-        port = "1234"
-        parent_pid = os.getppid()
+        self.cert_base64 = encode_cert_base64(self.cert)
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         grpc_controller_pb2_grpc.add_GRPCControllerServicer_to_server(
             ControllerServicer(server), server
         )
         tfplugin64_pb2_grpc.add_ProviderServicer_to_server(
-            ProviderServicer(), server
+            provider_servicer, server
         )
         key_cert_pair_for_grpc = (
             self.key.private_bytes(
@@ -44,17 +45,20 @@ class RPCPluginServer:
         creds = grpc.ssl_server_credentials([key_cert_pair_for_grpc])
         server.add_secure_port(f"127.0.0.1:{port}", creds)
         _configure_health_server(server)
-        cert_base64 = encode_cert_base64(self.cert)
-        server.start()
-        print(f"server listening on port {port}", file=stderr)
-        print(f"1|6|tcp|127.0.0.1:{port}|grpc|{cert_base64}", flush=True)
+        self.server = server
+
+    def run(self):
+        parent_pid = os.getppid()
+        self.server.start()
+        print(f"server listening on port {self.port}", file=stderr)
+        print_handshake_response(self.port, self.cert_base64)
         # RPCPlugin clients stop plugins with SIGKILL, which kills a process
         # immediately but leaves its children intact. Python RPCPlugin servers
         # will generally be launched by a bash script, so SIGKILL will only
         # kill that script's process but not the Python interpreter. A simple
         # way to detect this is to see if the parent PID has changed:
         while os.getppid() == parent_pid:
-            if not server.wait_for_termination(1):
+            if not self.server.wait_for_termination(1):
                 break
 
 
@@ -117,3 +121,7 @@ def encode_cert_base64(cert: x509.Certificate) -> str:
     return b64encode(cert.public_bytes(serialization.Encoding.DER)).decode(
         "ascii"
     )
+
+
+def print_handshake_response(port: str, cert_base64: str) -> None:
+    print(f"1|6|tcp|127.0.0.1:{port}|grpc|{cert_base64}", flush=True)
