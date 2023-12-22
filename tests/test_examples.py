bdafa0d1edb289d6b01df650d75256647a4f5301
Shahriar Heidrich <smheidrich@weltenfunktion.de>
2023-12-22 21:19:53 +0100

    Make tests generate .terraformrc => work anywhere

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,7 @@
 import subprocess as sp
+from os import environ
 from pathlib import Path
+from textwrap import dedent
 
 import pytest
 
@@ -14,24 +16,49 @@ def example_dir(request) -> Path:
     return Path("examples") / request.param
 
 
-# TODO currently, this only works if the user has configured ~/.terraformrc
-#   to point to the providers => fix
+@pytest.fixture()
+def tf_cli_config(tmp_path: Path) -> Path:
+    config_path = tmp_path / ".terraformrc"
+    config_contents = dedent(
+        f"""\
+    provider_installation {{
+      dev_overrides {{
+        "local/hello-world-provider-highlevel-async" = "{Path('examples/hello-world-provider-highlevel-async').absolute()}/terraform-plugin"
+        "local/hello-world-provider-highlevel" = "{Path('examples/hello-world-provider-highlevel').absolute()}/terraform-plugin"
+        "local/hello-world-provider-lowlevel" = "{Path('examples/hello-world-provider-lowlevel').absolute()}/terraform-plugin"
+      }}
+    }}
+    """.rstrip()
+    )
+    config_path.write_text(config_contents)
+    return config_path
 
 
-def test_plan(example_dir: Path) -> None:
+@pytest.fixture()
+def tf_cli_env(tf_cli_config: Path) -> dict[str, str]:
+    return {"TF_CLI_CONFIG_FILE": str(tf_cli_config)}
+
+
+def test_plan(example_dir: Path, tf_cli_env: dict[str, str]) -> None:
     tf_project_dir = example_dir / "terraform-project-using-provider"
-    sp.run(["terraform", "plan"], cwd=tf_project_dir, check=True)
+    env = environ.copy()
+    env.update(tf_cli_env)
+    sp.run(["terraform", "plan"], cwd=tf_project_dir, check=True, env=env)
 
 
-def test_destroy_apply(example_dir: Path) -> None:
+def test_destroy_apply(example_dir: Path, tf_cli_env: dict[str, str]) -> None:
     tf_project_dir = example_dir / "terraform-project-using-provider"
+    env = environ.copy()
+    env.update(tf_cli_env)
     # destroy (idempotent)
     sp.run(
         ["terraform", "destroy", "-auto-approve"],
         cwd=tf_project_dir,
         check=True,
+        env=env,
     )
     # apply
     sp.run(
-        ["terraform", "apply", "-auto-approve"], cwd=tf_project_dir, check=True
+        ["terraform", "apply", "-auto-approve"], cwd=tf_project_dir,
+        check=True, env=env,
     )
